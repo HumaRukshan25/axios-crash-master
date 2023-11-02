bde6413b5ed05abda4b00f14e71e7782aae14f9f
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-11-02 13:28:55 +0530

    done with all api calls axios

--- axios-crash-master/main.js
+++ axios-crash-master/main.js
@@ -1,180 +1,116 @@
+axios.defaults.headers.common['X-Auth-Token'] =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
 // GET REQUEST
 function getTodos() {
-  //axios({
+  // axios({
+  //   method: 'get',
+  //   url: 'https://jsonplaceholder.typicode.com/todos',
+  //   params: {
+  //     _limit: 5
+  //   }
+  // })
+  //   .then(res => showOutput(res))
+  //   .catch(err => console.error(err));
 
-    //method: 'get',
-    
-   // url: 'https://jsonplaceholder.typicode.com/todos',
-    //params:{
-     // _limit:5
-    //}
-    //})
-    
-  //  .then(res => showOutput(res))
-    
-    //.catch(err=> console.error(err));
-    
-    //}
-    axios
-    .get('https://jsonplaceholder.typicode.com/todos?_limit=5',{
-      timeout:5000
+  axios
+    .get('https://jsonplaceholder.typicode.com/todos?_limit=5', {
+      timeout: 5000
     })
-
-.then(res => showOutput (res))
-
-.catch(err=> console.error(err));
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  /*axios({
-
-    method: 'post',
-    
-   url: 'https://jsonplaceholder.typicode.com/todos',
-    data:{
-     title:'New Todo',
-     completed:false
-    }
-   })
-    
-   .then(res => showOutput(res))
-    
-    .catch(err=> console.error(err));
-    
-    }*/
-    axios
-
-.post('https://jsonplaceholder.typicode.com/todos', {
-
-title: 'New Todo',
-
-completed: false
-
-})
-
-.then(res => showOutput (res))
-
-.catch(err=> console.error(err));
+  axios
+    .post('https://jsonplaceholder.typicode.com/todos', {
+      title: 'New Todo',
+      completed: false
+    })
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
 }
+
 // PUT/PATCH REQUEST
 function updateTodo() {
   axios
-
-.patch('https://jsonplaceholder.typicode.com/todos/1', {
-
-title: 'update Todo',
-
-completed: true
-
-})
-
-.then(res => showOutput (res))
-
-.catch(err=> console.error(err));
+    .patch('https://jsonplaceholder.typicode.com/todos/1', {
+      title: 'Updated Todo',
+      completed: true
+    })
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
 }
+
 // DELETE REQUEST
 function removeTodo() {
   axios
-
-  .delete('https://jsonplaceholder.typicode.com/todos/1') 
-  
-  .then(res => showOutput (res))
-  
-  .catch(err=> console.error(err));
+    .delete('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
 }
 
 // SIMULTANEOUS DATA
 function getData() {
-  /*axios.all([
-
-    axios.get('https://jsonplaceholder.typicode.com/todos'),
-    
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-  ])
-    
-    .then(res => {
-    
-    console.log(res[0]);
-    
-    console.log(res [1]);
-    
-    showOutput(res [1]);
-    
-    })
-    
-    .catch(err=> console.error(err));
-}*/
-axios.all([
-axios.get('https://jsonplaceholder.typicode.com/todos_limit=5'),
-axios.get('https://jsonplaceholder.typicode.com/posts_limit=5')])
-.then(axios.spread((todos, posts) => showOutput(posts))) 
-.catch(err=> console.error(err));
+  axios
+    .all([
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+      axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+    ])
+    .then(axios.spread((todos, posts) => showOutput(posts)))
+    .catch(err => console.error(err));
 }
 
 // CUSTOM HEADERS
 function customHeaders() {
   const config = {
-
     headers: {
-    
-    'Content-Type': 'application/json',
-    
-    Authorization: 'sometoken'
-    
+      'Content-Type': 'application/json',
+      Authorization: 'sometoken'
     }
-    
-    };
-    
-    axios
-    
+  };
+
+  axios
     .post(
-    
-    'https://jsonplaceholder.typicode.com/todos',
-    {
-    title: 'New Todo',
-    
-    completed: false
-    
-    },
-    
-    config
+      'https://jsonplaceholder.typicode.com/todos',
+      {
+        title: 'New Todo',
+        completed: false
+      },
+      config
     )
     .then(res => showOutput(res))
-    
-    .catch(err=> console.error(err));
-    
-    }
+    .catch(err => console.error(err));
+}
+
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
   const options = {
-
     method: 'post',
-    
     url: 'https://jsonplaceholder.typicode.com/todos',
-    
     data: {
-    
-    title: 'Hello World'
+      title: 'Hello World'
     },
-    
-    transformResponse: axios.defaults.transformResponse.concat(data => {data.title = data.title.toUpperCase();
-    
-    return data;
-    
-    I})
-    
-    };
-    
-    axios(options).then(res => showOutput(res));
+    transformResponse: axios.defaults.transformResponse.concat(data => {
+      data.title = data.title.toUpperCase();
+      return data;
+    })
+  };
+
+  axios(options).then(res => showOutput(res));
 }
 
 // ERROR HANDLING
 function errorHandling() {
   axios
-    .get('https://jsonplaceholder.typicode.com/todos') // Corrected the URL to "todos"
-    .then((res) => showOutput(res))
-    .catch((err) => {
+    .get('https://jsonplaceholder.typicode.com/todos', {
+      // validateStatus: function(status) {
+      //   return status < 500; // Reject only if status is greater or equal to 500
+      // }
+    })
+    .then(res => showOutput(res))
+    .catch(err => {
       if (err.response) {
         // Server responded with a status other than 200 range
         console.log(err.response.data);
@@ -195,49 +131,46 @@ function errorHandling() {
 
 // CANCEL TOKEN
 function cancelToken() {
-  const CancelToken = axios.CancelToken;
-  const source = CancelToken.source();
+  const source = axios.CancelToken.source();
 
-  axios.get('https://jsonplaceholder.typicode.com/todos', {
-    cancelToken: source.token
-  })
-    .then((response) => {
-      console.log('Request completed:', response.data);
+  axios
+    .get('https://jsonplaceholder.typicode.com/todos', {
+      cancelToken: source.token
     })
-    .catch((error) => {
-      if (axios.isCancel(error)) {
-        console.log('Request canceled:', error.message);
-      } else {
-        console.log('Request error:', error);
+    .then(res => showOutput(res))
+    .catch(thrown => {
+      if (axios.isCancel(thrown)) {
+        console.log('Request canceled', thrown.message);
       }
     });
 
-  // To cancel the request, you can call source.cancel()
-  // source.cancel('Request canceled explicitly');
+  if (true) {
+    source.cancel('Request canceled!');
+  }
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
 axios.interceptors.request.use(
-  config => { 
-  console.log(
-  '${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}'
-);
-  return config;
+  config => {
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${
+        config.url
+      } at ${new Date().getTime()}`
+    );
+
+    return config;
   },
   error => {
-  
-  return Promise.reject(error);
-}
+    return Promise.reject(error);
+  }
 );
 
-// AXIOS INSTANCES
+// AXIOS INSTANCE
 const axiosInstance = axios.create({
-
-  
-  baseUrl: 'https://jsonplaceholder.typicode.com' });
-
-  
-  axiosInstance.get('/comments').then(res => showOutput(res));
+  // Other custom settings
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
+// axiosInstance.get('/comments').then(res => showOutput(res));
 
 // Show output in browser
 function showOutput(res) {
@@ -245,7 +178,6 @@ function showOutput(res) {
   <div class="card card-body mb-4">
     <h5>Status: ${res.status}</h5>
   </div>
-
   <div class="card mt-3">
     <div class="card-header">
       Headers
@@ -254,7 +186,6 @@ function showOutput(res) {
       <pre>${JSON.stringify(res.headers, null, 2)}</pre>
     </div>
   </div>
-
   <div class="card mt-3">
     <div class="card-header">
       Data
@@ -263,7 +194,6 @@ function showOutput(res) {
       <pre>${JSON.stringify(res.data, null, 2)}</pre>
     </div>
   </div>
-
   <div class="card mt-3">
     <div class="card-header">
       Config
